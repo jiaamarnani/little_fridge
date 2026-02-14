01128f4fcb4f9b214ee1947c7b99c70cd574ba1b
Jia Amarnani <jiaamarnani@gmail.com>
2026-02-14 00:40:27 -0500

    Inital commit

---
+++ littleFridge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  littleFridge
+//
+//  Created by Jia Amarnani on 2/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ littleFridge/littleFridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  littleFridgeApp.swift
+//  littleFridge
+//
+//  Created by Jia Amarnani on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct littleFridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
